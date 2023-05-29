f20416d4b58add97136c9438238ecb55aa105696
Assis <assisvascconcelos@gmail.com>
2023-05-28 23:51:16 -0300

    components dinamycs propertys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
-  styleUrls: ['./big-card.component.css']
+  styleUrls: ['./big-card.component.css'],
 })
 export class BigCardComponent {
-
+  photoCover: string =
+    'https://colunaboraviajar.com.br/wp-content/uploads/2023/05/Cena-deletada-de-Iron-Man-e-tao-vergonhosa-que-nunca-sera-lancada-diz-produtor.jpg';
+  cardTitle: string = 'NOVO HOMEM DE FERRO ANUNCIADO';
+  cardDescription: string =
+    'Marvel studios anuncia novo filme do homem de ferro.';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
-  styleUrls: ['./small-card.component.css']
+  styleUrls: ['./small-card.component.css'],
 })
 export class SmallCardComponent {
-
+  photoCover: string =
+    'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg';
+  cardTitle: string = 'NOVA SERIE ANUNCIADA';
 }
