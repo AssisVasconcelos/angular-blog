1c3a8d246f0587e215d40dfb613711b88065329d
Assis <assisvascconcelos@gmail.com>
2023-05-29 09:55:39 -0300

    components dinamycs properthys

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   templateUrl: './big-card.component.html',
   styleUrls: ['./big-card.component.css'],
 })
-export class BigCardComponent {
-  photoCover: string =
-    'https://colunaboraviajar.com.br/wp-content/uploads/2023/05/Cena-deletada-de-Iron-Man-e-tao-vergonhosa-que-nunca-sera-lancada-diz-produtor.jpg';
-  cardTitle: string = 'NOVO HOMEM DE FERRO ANUNCIADO';
-  cardDescription: string =
-    'Marvel studios anuncia novo filme do homem de ferro.';
+export class BigCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
+  @Input()
+  cardTitle: string = '';
+  @Input()
+  cardDescription: string = '';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   templateUrl: './small-card.component.html',
   styleUrls: ['./small-card.component.css'],
 })
-export class SmallCardComponent {
-  photoCover: string =
-    'https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg';
+export class SmallCardComponent implements OnInit {
+  @Input()
+  photoCover: string = '';
+  @Input()
   cardTitle: string = 'NOVA SERIE ANUNCIADA';
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
